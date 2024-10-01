d2f34d605d92dd15ac9ba8cfd84ecaee71fdc806
hamza1851 <hamza.dev011@gmail.com>
2024-10-01 12:36:39 +0530

    Add question-3

--- app.js
+++ app.js
@@ -3,31 +3,33 @@
 
 import http from "http"
 import fs from "fs"
+import uuid4 from "uuid4"
 const PORT = 3000
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" })
-
-  if (req.url === "/" && req.method === "GET") {
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" })
     fs.readFile("index.html", (err, data) => {
       if (err) {
-        res.writeHead(404)
-        res.write("Error: File not found")
+        res.writeHead(404, { "Content-Type": "text/plain" })
+        res.end("Error: File not found")
       } else {
-        res.write(data)
+        res.end(data)
       }
-      res.end()
     })
   } else if (req.method === "GET" && req.url === "/json") {
+    res.writeHead(200, { "Content-Type": "application/json" })
     fs.readFile("random.json", (err, data) => {
       if (err) {
-        res.writeHead(404)
-        res.write("Error: File not found")
+        res.writeHead(404, { "Content-Type": "text/plain" })
+        res.end("Error: File not found")
       } else {
-        res.write(data)
+        res.end(data)
       }
-      res.end()
     })
+  } else if (req.method === "GET" && req.url === "/uuid") {
+    const id = uuid4()
+    res.end(JSON.stringify({ uuid: id }))
   }
 })
 
